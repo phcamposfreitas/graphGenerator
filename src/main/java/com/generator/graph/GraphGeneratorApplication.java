49926b04f5009b9699ae22c6b67a6300f561b622
Paulo Henrique Campos de Freitas <paulopitangui@gmail.com>
2015-11-29 15:57:31 -0200

    Criando projeto Inicial

---
+++ src/main/java/com/generator/graph/GraphGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.generator.graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphGeneratorApplication.class, args);
+    }
+}
